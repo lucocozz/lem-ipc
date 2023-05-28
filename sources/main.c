c395dadc592ea4e2e25dc5376bfc6167e5bab280
lucocozz <lucocozz@student.42.fr>
2023-05-28 19:05:57 +0200

    update main

--- sources/main.c
+++ sources/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lcocozza <lcocozza@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lucocozz <lucocozz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 16:04:34 by lucocozz          #+#    #+#             */
-/*   Updated: 2023/05/25 15:00:37 by lcocozza         ###   ########.fr       */
+/*   Updated: 2023/05/28 18:44:20 by lucocozz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,17 @@
 
 int	main()
 {
+	t_player	*players;
+	t_polygon	*areas = NULL;
+	int			areas_len;
+
 	if (TEAMS_LEN < 2) {
 		fprintf(stderr, "Error: minimum of 2 teams is required\n");
 		return (EXIT_ERROR);
 	}
 
-	t_polygon *areas = NULL;
-	int areas_len = divide_board_equal_area(&areas, BOARD_WIDTH, BOARD_HEIGHT, TEAMS_LEN);
-	t_player *players = spread_players(areas, areas_len);
+	areas_len = divide_board_equal_area(&areas, BOARD_WIDTH, BOARD_HEIGHT, TEAMS_LEN);
+	players = spread_players(areas, areas_len);
 
 	print_board(players);
 
